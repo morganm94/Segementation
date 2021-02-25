586cdfd5ea24b122c3d28d93c4b6380ca50d1054
Morgan Mastrangelo <mmastrangelo1120@gmail.com>
2021-02-25 17:42:19 +0800

    Add files via upload

--- evaluate.py
+++ evaluate.py
@@ -5,22 +5,26 @@ import torch.nn.functional as F
 class Diceloss(nn.Module):
     def __init__(self):
         super().__init__()
-    def forward(self,prediction,target,smooth=1):
+    def forward(self,prediction,target,smooth=1e-12):
         assert prediction.shape==target.shape,'the dimensions of prediction and target do not match/diceloss'
-        intersection=(prediction.view(-1) * target.view(-1)).sum()
-        union=prediction.sum()+target.sum()
+        batch_size=prediction.shape[0]
+        intersection=(prediction.view(batch_size,-1) * target.view(batch_size,-1)).sum(dim=1)
+        union=prediction.view(batch_size,-1).sum(dim=1)+target.view(batch_size,-1).sum(dim=1)
         dice=(2*intersection+smooth)/(union+smooth)
-        dice_loss=1-dice
+        dice_loss = 1 - dice
+        dice_loss=torch.mean(dice_loss)
         return dice_loss
 
 class Dicecoeff(nn.Module):
     def __init__(self):
         super().__init__()
-    def forward(self,prediction,target,smooth=1):
+    def forward(self,prediction,target,smooth=1e-12):
         assert prediction.shape == target.shape, 'the dimensions of prediction and target do not match/dice'
-        intersection=(prediction.view(-1) * target.view(-1)).sum()
-        union=prediction.sum()+target.sum()
-        dice=(2*intersection+smooth)/(union+smooth)
+        batch_size = prediction.shape[0]
+        intersection = (prediction.view(batch_size, -1) * target.view(batch_size, -1)).sum(dim=1)
+        union = prediction.view(batch_size, -1).sum(dim=1) + target.view(batch_size, -1).sum(dim=1)
+        dice = (2 * intersection + smooth) / (union + smooth)
+        dice = torch.mean(dice)
         return dice
 
 class Iou(nn.Module):
@@ -32,17 +36,19 @@ class Iou(nn.Module):
         prediction[mask]=0#小于0.5的预测为False,在prediction——copy里小于0.5的被置为0
         mask=prediction>=0.5
         prediction[mask]=1
-        intersection=(prediction.view(-1)*target.view(-1)).sum()
-        union=target.sum()+prediction.sum()-intersection
+        batch_size = prediction.shape[0]
+        intersection=(prediction.view(batch_size,-1) * target.view(batch_size,-1)).sum(dim=1)
+        union=target.view(batch_size, -1).sum(dim=1)+prediction.view(batch_size, -1).sum(dim=1)-intersection
         iou=intersection/(union+smooth)
+        iou=torch.mean(iou)
         return iou
 
 def CE(input,target):
     assert input.shape==target.shape,'the dimensions of prediction and target do not match/BCE'
     return F.binary_cross_entropy(input,target)
 
-target=torch.eye(3)
-prediction=target
-
-bceloss=CE(prediction,target)
-print(bceloss)
+# target=torch.eye(3)
+# prediction=target
+#
+# bceloss=CE(prediction,target)
+# print(bceloss)
--- train.py
+++ train.py
@@ -6,24 +6,25 @@ from evaluate import *
 from readin import Brain_dataset
 
 
-device=torch.device('cudn:0' if torch.cuda.is_available() else 'cpu')
+device=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-EPOCH=25
-BATCHSIZE=10
+EPOCH=300
+BATCHSIZE=64
 PERCENTAGE=0.8
+NUM_WORKERS=4
 path='.\dataset\kaggle_3m'
-filter=[64,128,256,512,1024]
+filter=[32,64,128,256,512]
 Learning_rate=1e-3
 
 data=Brain_dataset(path)
 train_number=int(data.__len__()*PERCENTAGE)
 test_number=data.__len__()-train_number
 train_set,test_set=random_split(data,[train_number,test_number])
-train_loader=DataLoader(dataset=train_set,batch_size=BATCHSIZE,shuffle=True)
+train_loader=DataLoader(dataset=train_set,batch_size=BATCHSIZE,shuffle=True,num_workers=NUM_WORKERS)
 test_loader=DataLoader(dataset=test_set,batch_size=BATCHSIZE)
 
 unet=Unet(3,1,filter)
-unet.to(device)
+unet.to(device,dtype=torch.float)
 Loss_dsc=Diceloss()
 iou=Iou()
 dsc=Dicecoeff()
@@ -31,30 +32,38 @@ optimizer=torch.optim.Adam(unet.parameters(),lr=Learning_rate)
 
 
 for epoch in range(EPOCH):
-    print('Epoch {}/{}'.format(epoch+1,EPOCH))
-    running_loss=[]
+    print('EPOCH:{}/{}'.format(epoch+1,EPOCH))
+    history_loss={'train':[],'test':[]}
+    history_dsc={'train':[],'test':[]}
+    for phase in ['train','test']:
+        running_loss = 0.0
+        running_dsc = 0.0
+        if phase=='train':
+            unet.train()
+            now_loader=train_loader
+            number=train_number
+        else:
+            unet.eval()
+            now_loader = test_loader
+            number = test_number
+        for image, mask in now_loader:
+            image = image.to(device, dtype=torch.float)
+            mask = mask.to(device, dtype=torch.float)
+            with torch.set_grad_enabled(phase=='train'):
+                prediction = unet(image)
+                loss = Loss_dsc(prediction, mask)#try one loss firstly,
+                                            #in the seconde stage,i will attempt to
+                                            #add one more loss:CE
+            if phase=='train':
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+            running_loss+=loss.item()*image.size(0)
+            running_dsc+=dsc(prediction,mask)*image.size(0)
 
-    for image,mask in train_loader:
-        image=image.to(device,dtype=torch.float)
-        mask=mask.to(device,dtype=torch.float)
-        prediction=unet(image)
-        loss=Loss_dsc(prediction,mask)+CE(prediction,mask)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        running_loss.append(loss.item())
-
-dsc_list=[]
-iou_list=[]
-unet.eval()
-with torch.no_grad():
-    for img,msk in test_loader:
-        img=img.to(device,dtype=torch.float)
-        msk=msk.to(device,dtype=torch.float)
-        pred=unet(img)
-        iou_val=iou(img,msk)
-        dsc_val=dsc(img,msk)
-        dsc_list.append(dsc_val)
-        iou_list.append(iou_val)
-
-plt.plot(dsc_list,label='dsc')
+        epoch_loss=running_loss/number
+        epoch_dsc=running_dsc/number
+        print('{} loss:{}'.format(phase,epoch_loss))
+        print('{} dsc:{}'.format(phase, epoch_dsc))
+        history_loss[phase].append(epoch_loss)
+        history_dsc[phase].append(epoch_dsc)
